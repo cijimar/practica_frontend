220c47e94fbcf67b898a7835a6c803e71fe00f28
Cintia Jiménez <cijimar@gmail.com>
2026-05-19 19:40:48 +0200

    feat: inserta iconos genero

--- src/app/features/user-list/user-list.component.ts
+++ src/app/features/user-list/user-list.component.ts
@@ -1,26 +1,44 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import {Router} from "@angular/router";
+import { Router } from "@angular/router";
 import { UserPopupComponent } from '../user-popup/user-popup.component';
+import { Usuario } from 'src/app/core/models/user.model';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css'],
   standalone: true,
+  providers: [UserService],
   imports: [ CommonModule, UserPopupComponent ]
 })
+
 export class UserListComponent implements OnInit {
+
+  usuarios: Usuario [] = [];
   @Output() cerrarPopUpOk = new EventEmitter<void>();
   @Output() cerrarPopUpCancel = new EventEmitter<void>();
 
   modoPopup: String = 'CLOSED';
 
-  constructor(private router: Router) {
-
+  constructor(private router: Router, private userService: UserService) {      
   }
 
   ngOnInit(): void {
+
+    console.log("Cargando usuarios...");
+
+  this.userService.getAllUsers().subscribe({
+    next: (data) => {
+      console.log("DATA BACKEND:", data);
+      this.usuarios = data;
+    },
+    error: (error) => {
+      console.error("ERROR BACKEND:", error);
+    }
+  });
+
   }
 
   onCerrarPopUpOk() {
@@ -36,6 +54,6 @@ export class UserListComponent implements OnInit {
     this.modoPopup = 'LAUNCH';
   }
 
-  // @TODO: Implementar propiedades, atributos, métodos... necesarios para el funcionamiento del listado de usuarios
+ 
 
 }
